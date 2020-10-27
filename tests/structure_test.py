731d16bbd8457d3adf9fb1f3889759dcaf96f826
Thomas Nipen <tnipen@gmail.com>
2020-10-27 20:55:14 +0100

    More structure function and transform tests

--- tests/structure_test.py
+++ tests/structure_test.py
@@ -107,6 +107,22 @@ class Test(unittest.TestCase):
             with self.assertRaises(Exception) as e:
                 structure = gridpp.CrossValidation(barnes, dist)
 
+    def test_cross_validation(self):
+        structure = gridpp.BarnesStructure(1000)
+        structure_cv = gridpp.CrossValidation(structure, 2000)
+        p1 = gridpp.Point(0, 0, 0, 0, gridpp.Cartesian)
+        p2 = gridpp.Point(1000, 0, 0, 0, gridpp.Cartesian)
+        self.assertEqual(structure.corr(p1, p1), structure_cv.corr(p1, p1))
+        self.assertEqual(structure.corr(p1, p2), structure_cv.corr(p1, p2))
+        self.assertEqual(0.0, structure_cv.corr_background(p1, p2))
+        self.assertEqual(0.0, structure_cv.corr_background(p1, p1))
+
+        structure_cv = gridpp.CrossValidation(structure, 900)
+        self.assertEqual(structure.corr(p1, p1), structure_cv.corr(p1, p1))
+        self.assertEqual(structure.corr(p1, p2), structure_cv.corr(p1, p2))
+        self.assertEqual(structure_cv.corr(p1, p2), structure_cv.corr_background(p1, p2))
+        self.assertEqual(0, structure_cv.corr_background(p1, p1))
+
 
 if __name__ == '__main__':
     unittest.main()
